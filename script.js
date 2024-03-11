7ed83e8c2cced5cf286b643d15f1090b4dadfbda
arthurassists <apetbrz@gmail.com>
2024-03-11 19:50:47 -0400

    added mission info

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const corsproxy = "https://corsproxy.org/?";
 const worldstatefileurl = 'http://content.warframe.com/dynamic/worldState.php';
+const solnodesdata = "https://api.warframestat.us/solNodes/";
 
 const currenttimetext = document.getElementById("time");
 const refreshtimetext = document.getElementById("refresh");
+
+var solNodes;
+let xhttp = new XMLHttpRequest();
+xhttp.onload = function(){
+    solNodes = JSON.parse(this.responseText);
+}
+xhttp.open("GET",solnodesdata);
+xhttp.send();
 
 var e = document.getElementById("li1");
 e.innerHTML = "script working";
@@ -17,12 +26,13 @@ setCurrentTime();
 setInterval(setCurrentTime,1000);
 
 function refresh(){
-    let xhttp = new XMLHttpRequest();
+    xhttp = new XMLHttpRequest();
     
     xhttp.onload = function(){
-        console.log("data loaded at " + Date.now());
         let wfdata = JSON.parse(this.responseText);
-        refreshtimetext.innerHTML = "WORLDSTATE TIME: " + new Date(wfdata.Time * 1000).toLocaleTimeString();
+        let worldstatetime = new Date(wfdata.Time * 1000).toLocaleTimeString()
+        refreshtimetext.innerHTML = "WORLDSTATE TIME: " + worldstatetime;
+        console.log("data loaded at " + new Date().toLocaleTimeString() + " : worldstate time = " + worldstatetime);
         populatePage(wfdata);
     }
     xhttp.open("GET",corsproxy+worldstatefileurl);
@@ -43,7 +53,7 @@ function populatePage(data){
 
     e = document.getElementById("li5");
     e.innerHTML = "Checking for EXTERMINATE fissure:\t";
-    e.innerHTML += checkForFissure(data, "MT_EXTERMINATE");
+    e.innerHTML += checkForFissure(data, "MT_EXTERMINATION");
 
     e = document.getElementById("li6");
     e.innerHTML = "Checking for DISRUPTION fissure:\t";
@@ -54,16 +64,49 @@ function populatePage(data){
     e.innerHTML += checkForFissure(data, "MT_SURVIVAL");
 }
 
+function regionToPlanet(region){
+    switch(region){
+        case 0: return "Unknown";
+        case 1: return "Mercury";
+        case 2: return "Venus";
+        case 3: return "Earth";
+        case 4: return "Mars";
+        case 5: return "Jupiter";
+        case 6: return "Saturn";
+        case 7: return "Uranus";
+        case 8: return "Neptune";
+        case 9: return "Pluto";
+        case 10: return "Ceres";
+        case 11: return "Eris";
+        case 12: return "Sedna";
+        case 13: return "Europa";
+        case 14: return "Clan Dojo";
+        case 15: return "Void";
+        case 16: return "Phobos";
+        case 17: return "Deimos";
+        case 18: return "Lua";
+        case 19: return "Kuva Fortress";
+
+    }
+}
+function nodeToFaction(node){
+    return solNodes[node].enemy;
+}
+
 function checkForFissure(data, missionType){
     let fissures = data.ActiveMissions;
     let output = "no fissure active";
     
     fissures.forEach(mission => {
-        if(mission.MissionType == missionType){
+        if(mission.MissionType === missionType){
             let expirationTime = Number(mission.Expiry.$date.$numberLong);
 
             if(expirationTime > Date.now()){
                 output = "FISSURE ACTIVE! until: " + (new Date(Number(mission.Expiry.$date.$numberLong)).toLocaleTimeString());
+                output += "<br>relic: " + mission.Modifier;
+                output += "<br>planet: " + regionToPlanet(mission.Region);
+                output += "<br>steelpath: " + (mission.Hard != null ? "yes" : "no");
+                output += "<br>faction: " + nodeToFaction(mission.Node);
             }else{
                 output = "fissure ended! :(";
             }
